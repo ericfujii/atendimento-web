41d7269100e99b1f95051cb04264c0eeef5dd4f0
Eric Fujii <fujii.eric@gmail.com>
2015-06-04 19:58:19 -0300

    listagem

--- AtendimentoWeb/src/main/java/br/com/ericfujii/bean/ComidaListagemBean.java
+++ AtendimentoWeb/src/main/java/br/com/ericfujii/bean/ComidaListagemBean.java
@@ -5,10 +5,12 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
 
 import org.hibernate.Session;
 
 import br.com.ericfujii.entidade.ESituacaoPedido;
+import br.com.ericfujii.entidade.ItemPedido;
 import br.com.ericfujii.entidade.Produto;
 import br.com.ericfujii.hibernate.HibernateUtil;
 
@@ -23,6 +25,24 @@ public class ComidaListagemBean {
 	public void postConstruct() {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		produtos = (List<Produto>) session.createQuery("FROM Produto p JOIN FETCH p.produtoTipo pt WHERE pt.bebida = false ORDER BY p.nome ").list();
+	}
+	
+	public void atualizarItem(Integer idProduto, Integer idItem) {
+		for (Produto produto : produtos) {
+			if (produto.getId().equals(idProduto)) {
+				for (ItemPedido itemPedido : produto.getItensPedidos()) {
+					if (itemPedido.getId().equals(idItem)) {
+						Session session = HibernateUtil.getSessionFactory().openSession();
+				        session.beginTransaction();
+			        	session.update(itemPedido);
+			        	session.getTransaction().commit();
+			        	session.close();
+			        	break;
+					}
+				}
+				break;
+			}
+		}
 	}
 
 	public List<Produto> getProdutos() {
--- AtendimentoWeb/src/main/java/br/com/ericfujii/entidade/ESituacaoPedido.java
+++ AtendimentoWeb/src/main/java/br/com/ericfujii/entidade/ESituacaoPedido.java
@@ -1,8 +1,18 @@
 package br.com.ericfujii.entidade;
 
 public enum ESituacaoPedido {
-	NOVO,
-	AVISADO,
-	ENVIADO,
-	CANCELADO;
+	NOVO("#FFC800"),
+	AVISADO("#2895F1"),
+	ENVIADO("#809A00"),
+	CANCELADO("#DD4433");
+	
+	ESituacaoPedido(String corFundo) {
+		this.corFundo = corFundo;
+	}
+	
+	private String corFundo;
+
+	public String getCorFundo() {
+		return corFundo;
+	}
 }
