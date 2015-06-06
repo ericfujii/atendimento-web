0ace6d752f86d2c474283658e7d260dd81c0e6d6
Samara Santino <samara@nxmultiservicos.com.br>
2015-06-06 00:11:34 -0300

    ajuste servi?o rest

--- AtendimentoWeb/src/main/java/br/com/ericfujii/rest/RestServico.java
+++ AtendimentoWeb/src/main/java/br/com/ericfujii/rest/RestServico.java
@@ -1,41 +1,70 @@
 package br.com.ericfujii.rest;
 
-import java.util.ArrayList;
 import java.util.List;
-import br.com.ericfujii.entidade.ESituacao;
+import org.hibernate.Session;
 import br.com.ericfujii.entidade.ItemPedido;
 import br.com.ericfujii.entidade.Pedido;
 import br.com.ericfujii.entidade.Produto;
 import br.com.ericfujii.entidade.ProdutoTipo;
 import br.com.ericfujii.entidade.Usuario;
+import br.com.ericfujii.hibernate.HibernateUtil;
 
 public class RestServico {
+	
+	private StringBuilder selectProdutos = new StringBuilder();
+	{
+		selectProdutos.append("FROM Produto p ");
+		selectProdutos.append("JOIN FETCH p.produtoTipo pt ");
+		selectProdutos.append("ORDER BY p.ordem, p.id ");
+	}
+	
+	private StringBuilder selectProdutosTipos = new StringBuilder();
+	{
+		selectProdutosTipos.append("FROM ProdutoTipo pt ");
+	}
+	
+	private StringBuilder selectUsuarios = new StringBuilder();
+	{
+		selectUsuarios.append("FROM Usuario u ");
+	}
+	
+	private StringBuilder selectPedidos = new StringBuilder();
+	{
+		selectUsuarios.append("FROM Pedido p ");
+	}
+	
+	private StringBuilder selectItensPedidos = new StringBuilder();
+	{
+		selectUsuarios.append("FROM ItemPedido ip ");
+	}
 
+	@SuppressWarnings("unchecked")
 	public List<ProdutoTipo> obterProdutosTipos() {
-		ProdutoTipo pt = new ProdutoTipo();
-		pt.setId(1);
-		pt.setBebida(false);
-		pt.setNome("TESTE");
-		pt.setSituacao(ESituacao.ATIVO);
-		List<ProdutoTipo> produtosTipos = new ArrayList<ProdutoTipo>();
-		produtosTipos.add(pt);
-		return produtosTipos;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		return session.createQuery(selectProdutosTipos.toString()).list();
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Produto> obterProdutos() {
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		return session.createQuery(selectProdutos.toString()).list();
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Usuario> obterUsuarios() {
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		return session.createQuery(selectUsuarios.toString()).list();
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Pedido> obterPedidos() {
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		return session.createQuery(selectPedidos.toString()).list();
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<ItemPedido> obterItensPedidos() {
-		return null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		return session.createQuery(selectItensPedidos.toString()).list();
 	}
-
 }
